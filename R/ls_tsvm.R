a28f9a4b0d7c91761c987dea435a357737a805f1
define957 <zhangjiaqi957957@outlook.com>
2025-01-06 02:41:05 +0800

    Update linear systems solver

--- R/ls_tsvm.R
+++ R/ls_tsvm.R
@@ -9,8 +9,8 @@ ls_tsvm_dual_solver <- function(KernelX, idx, C1, C2) {
   GramE <- t(EMat) %*% EMat
   e1 <- matrix(1, Fn)
   e2 <- matrix(1, Mn)
-  u <- -chol2inv(chol(GramF + GramE/C1 + diag(1e-7, xp))) %*% t(FMat) %*% e1
-  v <-  chol2inv(chol(GramE + GramF/C2 + diag(1e-7, xp))) %*% t(EMat) %*% e2
+  u <- -cholsolve(GramF + GramE/C1 + diag(1e-7, xp), t(FMat) %*% e1)
+  v <-  cholsolve(GramE + GramF/C2 + diag(1e-7, xp), t(EMat) %*% e2)
   BaseDualHingeTSVMClassifier <- list("coef1" = as.matrix(u),
                                       "coef2" = as.matrix(v))
   return(BaseDualHingeTSVMClassifier)
@@ -43,7 +43,7 @@ ls_tsvm <- function(X, y, C1 = 1, C2 = C1,
                     kernel = c("linear", "rbf", "poly"),
                     gamma = 1 / ncol(X), degree = 3, coef0 = 0,
                     solver = c("dual"), fit_intercept = TRUE,
-                   randx = 0.1, ...) {
+                    randx = 0.1, ...) {
   X <- as.matrix(X)
   y <- as.matrix(y)
   class_set <- sort(unique(y))
